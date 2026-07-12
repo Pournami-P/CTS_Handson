da2b905740d5c1bc25c5e00a8f930322acb88eaa
Pournami <pournamip2005@gmail.com>
2026-07-12 14:52:11 +0530

    Completed Maven Project Configuration

---
+++ Week2/LibraryManagementMaven/src/main/java/com/library/LibraryManagementMaven/LibraryManagementMavenApplication.java
@@ -0,0 +1,13 @@
+package com.library.LibraryManagementMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementMavenApplication.class, args);
+	}
+
+}
---
+++ Week2/LibraryManagementMaven/src/test/java/com/library/LibraryManagementMaven/LibraryManagementMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.LibraryManagementMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
